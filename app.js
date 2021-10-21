901981fd4074d635cd1e1bb912ed6cdbb33b0a9f
micheaol <micheaol80@gmail.com>
2021-10-21 01:09:36 +0100

    feat: add logic to save blog to the db and get blogs from db

--- app.js
+++ app.js
@@ -1,16 +1,62 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
+//Create express app
 const app = express();
 
+//Connection string for db:
+const dbURI = "mongodb+srv://nodeninja:test1234@blog-tuts.tnx6t.mongodb.net/node-tuts?retryWrites=true&w=majority";
+
+//Connect Db with mongoose:
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then((result) => app.listen(3000))
+        .catch((err) => console.log(err))
 
 //set up view engine:
 app.set('view engine', 'ejs')
 
-//listening to the request:
-app.listen(3000);
-
 //accessing static file:
 app.use(express.static('public'));
+
+//mongoose and mongodb sandbox:
+app.get('/add-blog', (req, res)=>{
+    const blog = new Blog({
+        title: "Seriously Getting it",
+        snipet: "I am on the right path with node.js",
+        blog: "I am seriously trying all I can to ensure I learn node.js"
+    });
+
+    blog.save()
+        .then((result)=>{
+            res.send(result)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+});
+
+//Get all blogs:
+app.get('/all-blogs', (req, res)=>{
+    Blog.find()
+        .then((result)=>{
+            res.send(result)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+});
+
+//Get single blog:
+app.get('/blog-id', (req, res)=>{
+    Blog.findById("6170aa4de53604bfdee3eeaa")
+        .then((result) =>{
+            res.send(result)
+        })
+        .catch((err) =>{
+            console.log(err)
+        })
+})
 
 app.get('/', (req, res)=>{
     const blogs = [
---
+++ models/blog.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const blogSchema = Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snipet: {
+        type: String,
+        required: true
+    },
+    blog: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
